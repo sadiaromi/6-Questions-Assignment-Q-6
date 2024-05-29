d722b06902b391620ffcfd30c8457e49e10327fc
sadiaromi <sadiaroman28@gmail.com>
2024-05-29 19:52:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//Anonymous function
+const student = function (name) {
+    console.log(`My name is ${name}`);
+};
+student("Sadia");
+// Normal function
+function student1(name1) {
+    console.log(`My name is ${name1}`);
+}
+student1("Sana");
+export {};
---
+++ main.ts
@@ -0,0 +1,12 @@
+//Anonymous function
+const student = function (name: string){
+    console.log(`My name is ${name}`);
+}
+student("Sadia");
+
+
+// Normal function
+function student1  (name1: string){
+    console.log(`My name is ${name1}`);
+}
+student1("Sana");
